62b4a7dd4b0e2740c579f379435820e9a4f82412
JayachandranSM <46161255+JayachandranSM@users.noreply.github.com>
2023-11-20 20:25:00 +0530

    Add files via upload

--- stockFunctions.py
+++ stockFunctions.py
@@ -1,3 +1,18 @@
+def split_sequences(sequences, n_steps_in, n_steps_out):
+    X, y = list(), list()
+    for i in range(len(sequences)):
+    # find the end of this pattern
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out
+        # check if we are beyond the dataset
+        if out_end_ix > len(sequences):
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix:out_end_ix, :]
+        X.append(seq_x)
+        y.append(seq_y)
+        from numpy import array
+    return array(X), array(y)
 
 def conversion(y_train,stk_data):
     import pandas as pd
